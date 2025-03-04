f24e8667fd61c260b4e5e81befe3912b7a349d35
numankaradavut <Karadavu@students.uni-marburg.de>
2025-03-04 12:17:54 +0100

    Integrated chess pieces and game logic into ChessMania

--- MenuScripts/menu.gd
+++ MenuScripts/menu.gd
@@ -2,8 +2,7 @@ extends Control
 
 
 func _on_standard_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://board.tscn")
 
 func _on_tamerlane_pressed() -> void:
 	pass # Replace with function body.
