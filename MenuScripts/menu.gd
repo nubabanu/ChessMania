2820c7d912c5469b03028ba585a876ed71a943b6
numankaradavut <Karadavu@students.uni-marburg.de>
2025-03-04 11:36:36 +0100

    Initial commit

---
+++ MenuScripts/menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_standard_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_tamerlane_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_total_war_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_fisher_pressed() -> void:
+	pass # Replace with function body.
