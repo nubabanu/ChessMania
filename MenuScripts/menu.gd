ab583c597a5814de08622358a61b73d8cf475514
Sicim <Sicim@students.uni-marburg.de>
2025-03-13 12:09:24 +0100

    AI Toggle Button and Back Button

--- MenuScripts/menu.gd
+++ MenuScripts/menu.gd
@@ -30,3 +30,7 @@ func _on_fisher_pressed() -> void:
 
 func _on_toggled(toggled_on: bool) -> void:
 	GameSettings.is_single_player = not GameSettings.is_single_player
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenesMenu/menu.tscn")
